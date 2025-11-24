3ddef8711712140311af4e19539d2b87a901e761
Vedanti5 <ganvy010@mymail.unisa.edu.au>
2025-11-24 22:57:30 +1030

    Made all the possible changes in the zookeeper file

--- zookeeper.py
+++ zookeeper.py
@@ -1,12 +1,14 @@
 '''
-File: filename.py
+File: zookeeper.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author110439713
+Username: ganvy010
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+from staff import StaffMember
 class Zookeeper(StaffMember):
-    def feed(self,):
+    def __init__(self, name):
+        super().__init__(name, role="zookeeper")
 
+    # zookeeper-specific convenience methods could be added here
