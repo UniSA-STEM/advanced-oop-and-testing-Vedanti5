621302b4414d001bb7a92dfaf156b474b6b19e48
Vedanti5 <ganvy010@mymail.unisa.edu.au>
2025-11-24 17:01:44 +1030

    Made the animal class outline with some def functions.

--- animal.py
+++ animal.py
@@ -5,4 +5,17 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Animal:
+    name: str
+    species: str
+    age: int
+    dietary_needs: str
+    category: str
+
+    def eat(self, amount:):
+
+    def sleep(self, hours:):
+
+    def making_sounds(self,):
