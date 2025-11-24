9fe59d5597f2a37f049d35f1d34192c67560f0fa
Vedanti5 <ganvy010@mymail.unisa.edu.au>
2025-11-24 22:57:51 +1030

    Made all the possible changes in the enclosure file

--- enclosure.py
+++ enclosure.py
@@ -1,10 +1,10 @@
 '''
-File: filename.py
+File: enclosure.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
-This is my own work as defined by the University's Academic Integrity Policy.
+Author: Vedanti Ganjale
+ID: 110439713
+Username: ganvy010
+This is Vedanti Ganjale my own work as defined by the University's Academic Integrity Policy.
 '''
 
 
