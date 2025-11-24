0000fec5a1bbb4c94a389ab83cdf3e156095bc47
Vedanti5 <ganvy010@mymail.unisa.edu.au>
2025-11-24 17:10:34 +1030

    Made the enclouser class outline with some important def functions.

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,25 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclouser:
+    id: str
+    size_sqm: float
+    environment: str
+    allowed_category: str
+    cleanliness: float = 100.0
+    animals: List[Animal] = field(default_factory = list)
+    max_animals: int = None
+
+    def add_animal(self,):
+
+
+    def remove_animal(self,):
+
+
+    def list_animal(self,):
+
+    def clean(self,):
+
+
